933584dacd1cc2e3719f99f40a749b1a1b31a5a8
Athouba-Pebam <71960090+Athouba-Pebam@users.noreply.github.com>
2021-08-25 08:31:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+    loadImage = "photo-1522771739844-6a9f6d5f14af.jpeg"
+}
+
+function setup(){
+    canvas = createCanvas(640, 440);
+    canvas.center();
+}
+
+function draw(){
+}
+function back_button(){
+    window.location = "bedrom.html";
+}
